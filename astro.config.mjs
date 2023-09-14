94996967269d3f5c696e67fbf78dfaf127440250
Ric Dodong <ricalgen@gmail.com>
2023-09-13 20:34:30 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ricdodong.github.io',
+	site: 'https://ricdodong.github.io/home',
 	integrations: [mdx(), sitemap()],
 });
