0a2a6890168f25202a7a184489cc9cec3b182766
Ric Dodong <ricalgen@gmail.com>
2023-09-13 20:41:19 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://ricdodong.github.io/home',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://astronaut.github.io',
+  base: '/home',
+})
